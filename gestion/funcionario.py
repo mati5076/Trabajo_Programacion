88557372b0ab568a66036856faeeb3390b3c513c
Matias Montecinos <montecinosmatias142@gmail.com>
2023-11-11 22:42:37 -0300

    ,

--- gestion/funcionario.py
+++ gestion/funcionario.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import messagebox
+from gestion.solicitud import solicitud
 
 class Funcionario:
     def __init__(self):
@@ -8,35 +10,43 @@ class Funcionario:
         self.nombre_completo_label = tk.Label(self.ventana_funcionario, text="Nombre completo:")
         self.nombre_completo_label.pack()
         
-        self.nombre_completo = tk.Entry()
+        self.nombre_completo = tk.Entry(self.ventana_funcionario)
         self.nombre_completo.pack()
         
         self.run_label = tk.Label(self.ventana_funcionario , text="Run:")
         self.run_label.pack()
         
-        self.run = tk.Entry()
+        self.run = tk.Entry(self.ventana_funcionario)
         self.run.pack()
 
         self.cargo_label = tk.Label(self.ventana_funcionario , text="Cargo")
         self.cargo_label.pack()
 
-        self.cargo = tk.Entry()
+        self.cargo = tk.Entry(self.ventana_funcionario)
         self.cargo.pack()
 
         self.empresa_label = tk.Label(text="Empresa")
         self.empresa_label.pack()
 
-        self.empresa = tk.Entry()
+        self.empresa = tk.Entry(self.ventana_funcionario)
         self.empresa.pack()
 
         opcion_contrato = ["plazo fijo","Indefinido"]
         self.tipo = tk.StringVar()
         self.tipo.set(" ")
         
-        self.contrato_label = tk.Label(text="Contrato:")
+        self.contrato_label = tk.Label(self.ventana_funcionario,text="Contrato:")
         self.contrato_label.pack()
         
         self.contrato = tk.OptionMenu(self.ventana_funcionario,self.tipo,*opcion_contrato)
         self.contrato.pack()
-        self.contrato.config()
+
+        def boton_func():
+           messagebox.showinfo("Hola" , "asdasd")
+           self.ventana_funcionario.withdraw()
+           solicitud()
+
+        self.boton = tk.Button(self.ventana_funcionario, text="preciona" ,command=boton_func) 
+        self.boton.pack()
+
         self.ventana_funcionario.mainloop()
--- gestion/solicitud.py
+++ gestion/solicitud.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox
-from gestion.funcionario import Funcionario
 
 class solicitud:
     def __init__(self):
@@ -10,34 +8,26 @@ class solicitud:
         self.nombre_solicitante_label = tk.Label(self.ventan_solicitante,text="Nombre_solicitante")
         self.nombre_solicitante_label.pack()
 
-        self.nombre_solicitante = tk.Entry()
+        self.nombre_solicitante = tk.Entry(self.ventan_solicitante)
         self.nombre_solicitante.pack()
 
         self.nombre_responsable_label = tk.Label(self.ventan_solicitante,text="Nombre_responsable:")
         self.nombre_responsable_label.pack()
 
-        self.nombre_responsable = tk.Entry()
+        self.nombre_responsable = tk.Entry(self.ventan_solicitante)
         self.nombre_responsable.pack()
 
-        self.fecha_solicitante_label = tk.Label()
+        self.fecha_solicitante_label = tk.Label(self.ventan_solicitante , text="fecha")
         self.fecha_solicitante_label.pack()
 
-        self.fecha_solicitante = tk.Entry()
+        self.fecha_solicitante = tk.Entry(self.ventan_solicitante)
         self.fecha_solicitante.pack()
 
-        self.fecha_devolucion_label = tk.Label()
+
+        self.fecha_devolucion_label = tk.Label(self.ventan_solicitante,text="devuelve")
         self.fecha_devolucion_label.pack()
 
-        self.fecha_devolucion = tk.Entry()
+        self.fecha_devolucion = tk.Entry(self.ventan_solicitante)
         self.fecha_devolucion.pack()
 
-        def boton_func():
-           messagebox.showinfo("Hola" , "asdasd")
-           Funcionario()
-
-        self.boton = tk.Button(self.ventan_solicitante, text="preciona" ,command=boton_func) 
-        self.boton.pack()
         self.ventan_solicitante.mainloop()
-
-
-solicitud()
